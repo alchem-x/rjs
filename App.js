2b33c8214135b915e27b5d67e28cbcff9c5425cd
cbdyzj <cbdyzj@jianzhao.org>
2021-10-09 22:14:04 +0800

    Amend

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
+function random255() {
+    return Math.trunc(256 * Math.random())
+}
+
 define((require) => {
-    const { css } = require('emotion')
+    const { css } = require('@emotion/css')
 
     const _App = css`
       box-sizing: border-box;
@@ -7,10 +11,39 @@ define((require) => {
       min-height: 100vh;
       text-align: center;
       padding: 20vh;
+
+      & > span {
+        border: .5rem solid;
+        border-radius: 2px;
+        padding: .5rem;
+        font-weight: bold;
+        cursor: pointer;
+        font-family: sans-serif;
+      }
     `
     return {
         template: `
-          <div class="${_App}">rjs</div>
+          <div class="${_App}">
+          <span :style="textStyle" @click="setRandomColor">rjs</span>
+          </div>
         `,
+        data() {
+            return {
+                color: '',
+            }
+        },
+        mounted() {
+            this.setRandomColor()
+        },
+        computed: {
+            textStyle() {
+                return `color: ${this.color};`
+            }
+        },
+        methods: {
+            setRandomColor() {
+                this.color = `rgb(${random255()},${random255()},${random255()})`
+            },
+        }
     }
 })
--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 requirejs.config({
     enforceDefine: true,
     paths: {
-        Vue: 'https://unpkg.com/vue@2.6.14/dist/vue.min',
-        emotion: 'https://unpkg.com/@emotion/css@11.1.3/dist/emotion-css.umd.min',
+        vue: 'https://unpkg.com/vue@2.6.14/dist/vue.min',
+        '@emotion/css': 'https://unpkg.com/@emotion/css@11.1.3/dist/emotion-css.umd.min',
     },
 })
 
 define((require) => {
-    const Vue = require('Vue')
+    const Vue = require('vue')
     const App = require('./App.js')
 
     const app = new Vue({
