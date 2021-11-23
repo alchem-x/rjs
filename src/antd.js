fdb81f97cd6ad1ac3a54fcbd388d7ebc7045e29e
Cbdy <cbdyzj@jianzhao.org>
2021-11-23 13:01:01 +0800

    Update antd.js

--- src/antd.js
+++ src/antd.js
@@ -1,5 +1,5 @@
 define((require) => {
-    const loadCss = require('./loadCss.js')
+    const loadCss = require('./loadCss')
     const antd = require('https://unpkg.com/antd@4.17.1/dist/antd.min.js')
     const icons = require('https://unpkg.com/@ant-design/icons@4.7.0/dist/index.umd.min.js')
 
